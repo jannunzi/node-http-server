e83727af7784544382b1e82a85e6ba772626e579
Jose Annunziato <jose.annunziato@sonos.com>
2023-03-19 08:41:36 -0400

    added port

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-import express from 'express'
-const app = express()
-app.get('/', (req, res) => {
-    res.send('<h1>Welcome!</h1>')
-})
-app.get('/hello', (req, res) => {
-    res.json({message: 'Hello World!'})
-})
-const PORT = 4000
-app.listen(PORT)
+import express from "express";
+const app = express();
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome!</h1>");
+});
+app.get("/hello", (req, res) => {
+  res.json({ message: "Hello World!" });
+});
+const port = process.env.PORT || 4000;
+app.listen(port);
